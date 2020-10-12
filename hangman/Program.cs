b575b09af4d6c9cf29c60ec3f66dcc2eae89c109
Björn Hallberg <bjorn.hallberg@hm.com>
2020-10-12 14:48:29 +0200

    Template for output on console

--- hangman/Program.cs
+++ hangman/Program.cs
@@ -6,7 +6,85 @@ namespace hangman
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteHangman();
+        }
+
+        static void WriteHangman()
+        {
+            Console.Clear();
+            Console.WriteLine();
+            DrawHangman(1);
+            Console.WriteLine();
+            Console.WriteLine($" MAURITZ");
+            Console.WriteLine();
+            Console.WriteLine($" A S I K B");
+            Console.WriteLine();
+            Console.WriteLine($" Guesses left: 6");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($" You won!");
+            Console.ResetColor();
+        }
+
+        static void DrawHangman(int image)
+        {
+            switch (image)
+            {
+                case 1:
+                    Console.WriteLine("   +---+");
+                    Console.WriteLine("   |   |");
+                    Console.WriteLine("       |");
+                    Console.WriteLine("       |");
+                    Console.WriteLine("       |");
+                    Console.WriteLine("       |");
+                    Console.WriteLine(" =========");
+                    break;
+                case 2:
+                    Console.WriteLine("   +---+");
+                    Console.WriteLine("   |   |");
+                    Console.WriteLine("   O   |");
+                    Console.WriteLine("       |");
+                    Console.WriteLine("       |");
+                    Console.WriteLine("       |");
+                    Console.WriteLine(" =========");
+                    break;
+                case 3:
+                    Console.WriteLine("   +---+");
+                    Console.WriteLine("   |   |");
+                    Console.WriteLine("   O   |");
+                    Console.WriteLine("   |   |");
+                    Console.WriteLine("       |");
+                    Console.WriteLine("       |");
+                    Console.WriteLine(" =========");
+                    break;
+                case 4:
+                    Console.WriteLine("   +---+");
+                    Console.WriteLine("   |   |");
+                    Console.WriteLine("   O   |");
+                    Console.WriteLine("  /|   |");
+                    Console.WriteLine("       |");
+                    Console.WriteLine("       |");
+                    Console.WriteLine(" =========");
+                    break;
+                case 5:
+                    Console.WriteLine("   +---+");
+                    Console.WriteLine("   |   |");
+                    Console.WriteLine("   O   |");
+                    Console.WriteLine("  /|\\  |");
+                    Console.WriteLine("  /    |");
+                    Console.WriteLine("       |");
+                    Console.WriteLine(" =========");
+                    break;
+                case 6:
+                    Console.WriteLine("   +---+");
+                    Console.WriteLine("   |   |");
+                    Console.WriteLine("   O   |");
+                    Console.WriteLine("  /|\\  |");
+                    Console.WriteLine("  / \\  |");
+                    Console.WriteLine("       |");
+                    Console.WriteLine(" =========");
+                    break;
+            }
         }
     }
 }
