8b1ed174a45a315697dd4563df4b7a8f20f17fe8
HM\MAGAO <maja.gabrielsson@hm.com>
2020-10-12 14:51:29 +0200

    added method guessCompare

--- hangman/Program.cs
+++ hangman/Program.cs
@@ -6,7 +6,34 @@ namespace hangman
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            char guess = 'o';
+            string word = "Sommar";
+            Console.WriteLine();
+            GuessCompare(guess, word);
+        }
+
+
+
+        static void GuessCompare(char guess, string word){
+            bool correctGuess = false;
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (guess == word[i])
+                {
+                    Console.WriteLine(guess);
+                    correctGuess = true;
+                }
+                
+            }
+            if (correctGuess == false)
+            {
+                Console.WriteLine("Not a correct guess");
+            }
+            else if (correctGuess)
+            {
+                Console.WriteLine("Correct");
+            }
+
         }
     }
 }
